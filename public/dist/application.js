9947119b48be59135bf63549f7c65c200ecc3674
Elmer Meza <meza.elmer@gmail.com>
2015-02-07 22:08:13 -0500

    production build of last update
    update dist folder

--- public/dist/application.js
+++ public/dist/application.js
@@ -166,8 +166,8 @@ angular.module('core').controller('HeaderController', ['$scope', '$location', '$
 .controller('ModalInstanceCtrl', 
   ['$scope', '$filter', '$modalInstance', 'Agencies',
   function($scope, $filter, $modalInstance, Agencies) {
-  	Agencies.query(function(admin) {
-  		var admin = $filter('filter')(admin, {username: 'AAA'});
+  	Agencies.query(function(users) {
+  		var admin = $filter('filter')(users, {username: 'AAA'});
 		$scope.dueDate = $filter('date')(admin[0].due, 'fullDate');
 	});
 
@@ -254,7 +254,7 @@ angular.module('letters').controller('ArticlesController',
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
-		};
+		}
 
 		//Check out the file the user is trying to upload
 		function fileInfo(e){
@@ -300,8 +300,8 @@ angular.module('letters').controller('ArticlesController',
 					var teen_col	= headers.indexOf('Accepted Teens');
 					var seniors_col	= headers.indexOf('Accepted Seniors');
 				  
-					for(var i=1; i<rows.length; i++){
-						var record = rows[i].split(',');
+					for(var x=1; x<rows.length; x++){
+						var record = rows[x].split(',');
 						if($filter('filter')($scope.partners, {username: record[code_col]}).length === 0) {
 							var newPartner = {
 								username: 	record[code_col],
@@ -553,45 +553,60 @@ angular.module('letters')
 
 		});
 
-
 }]);
 'use strict';
 
 angular.module('letters').controller('AgencyController', 
-	['$scope', '$stateParams', '$location', '$filter' ,'$modal', 'Authentication', 'Articles', 'Agencies', 'Users',
-	function($scope, $stateParams, $location, $filter, $modal, Authentication, Articles, Agencies, Users) {
-		$scope.authentication = Authentication;
+	['$scope', '$stateParams', '$location', '$filter' ,'$timeout', 'Authentication', 'Articles', 'Agencies', 'Users',
+	function($scope, $stateParams, $location, $filter, $timeout, Authentication, Articles, Agencies, Users) {
+		$scope.user = Authentication.user;
 
-		if (!$scope.authentication.user) $location.path('/');
+		if (!$scope.user) $location.path('/');
 
-		$scope.adminView = $scope.authentication.user.username === 'AAA' ? true : false;
+		$scope.adminView = $scope.user.username === 'AAA' ? true : false;
 		$scope.minAge = null;
 		$scope.maxAge = null; 
-		$scope.allValid = true;
-		$scope.blankName = false;
-		$scope.invalidGender = false;
-		$scope.invalidAge = false;
-		$scope.currentIndex = 0;
-		$scope.activateAlert = false;
 		$scope.recipients = null;
 		$scope.tabs = [];
 		var Recipients = null;
 		var blankRecords = null;
-		var due = null;
+		var currentIndex = 0;
 
 		//Helps initialize page by finding the appropriate letters
 		$scope.find = function() {
 			$scope.letters = Articles.query(function() {
 				if($scope.adminView) {
 					$scope.currentAgency = Agencies.get({agencyId: $stateParams.articleId}, function() {
-						due = $filter('date')(Authentication.user.due, 'MM/dd/yy');
 						init();
 					});
 				}
 				else {
-					$scope.currentAgency = $scope.authentication.user;
-					Agencies.query(function(admin) {
-						due = $filter('date')(admin[0].due, 'MM/dd/yy');
+					$scope.currentAgency = $scope.user;
+					Agencies.query(function(users) {
+						var admin = $filter('filter')(users, {username: 'AAA'});
+						var due = $filter('date')(admin[0].due, 'MM/dd/yy');
+
+						if($scope.currentAgency.status < 3) {
+							var countdown = dateDiff(new Date(), new Date(admin[0].due));
+							if(countdown === 14) {
+								$scope.alert = {active: true, type: 'warning', msg: 'Two weeks left'};
+							}
+							else if(countdown === 7) {
+								$scope.alert = {active: true, type: 'warning', msg: 'One week left'};
+							}
+							else if(countdown === 0) {
+								$scope.alert = {active: true, type: 'danger', msg: 'Last day to submit'};
+							}
+							else if(countdown === 1) {
+								$scope.alert = {active: true, type: 'danger', msg: 'One day left'};
+							}
+							else if(countdown < 0) {
+								$scope.alert = {active: true, type: 'danger', msg: 'Past due -- please submit it ASAP'};
+							}
+							else if(countdown <= 3) {
+								$scope.alert = {active: true, type: 'danger', msg: countdown + ' days left'};
+							}
+						}
 						init();
 					});
 				}
@@ -600,33 +615,28 @@ angular.module('letters').controller('AgencyController',
 
 		function init() {
 			Recipients = $filter('filter')($scope.letters, {track: $scope.currentAgency.username});
-					var myChildren = $filter('filter')(Recipients, {track: $scope.currentAgency.username + 'C'});
-					var myTeens = $filter('filter')(Recipients, {track: $scope.currentAgency.username + 'T'});
-					var mySeniors = $filter('filter')(Recipients, {track: $scope.currentAgency.username + 'S'});
+			var myChildren = $filter('filter')(Recipients, {track: $scope.currentAgency.username + 'C'});
+			var myTeens = $filter('filter')(Recipients, {track: $scope.currentAgency.username + 'T'});
+			var mySeniors = $filter('filter')(Recipients, {track: $scope.currentAgency.username + 'S'});
 
-					$scope.tabs = [
-						{ title:'Children', content: myChildren, active: false },
-						{ title:'Teens', content: myTeens, active: false },
-						{ title:'Seniors', content: mySeniors, active: false }
-					];
+			$scope.tabs = [
+				{ title:'Children', content: myChildren, active: false },
+				{ title:'Teens', content: myTeens, active: false },
+				{ title:'Seniors', content: mySeniors, active: false }
+			];
 
-					$scope.activateTab(myChildren.length > 0 ? $scope.tabs[0] : (myTeens.length > 0 ? $scope.tabs[1] : $scope.tabs[2]));
+			$scope.activateTab(myChildren.length > 0 ? $scope.tabs[0] : (myTeens.length > 0 ? $scope.tabs[1] : $scope.tabs[2]));
 
-					if((!$scope.adminView && $scope.currentAgency.status >= 3) || ($scope.adminView && $scope.currentAgency.status == 5)) downloadCSV();
+			if((!$scope.adminView && $scope.currentAgency.status >= 3) || ($scope.adminView && $scope.currentAgency.status === 5)) downloadCSV();
 		}
 		
 		//Allows user to work on another tab
 		$scope.activateTab = function(clicked) {
 			clicked.active = true;
 			$scope.recipients = clicked.content;
-			blankRecords = $filter('filter')($scope.recipients, {name: ''});
-			$scope.currentIndex = blankRecords.length > 0 ? $scope.recipients.indexOf(blankRecords[0]) : 0;
+			blankRecords = $filter('filter')($scope.recipients, function(rec) { return !rec.name; });
+			currentIndex = blankRecords.length > 0 ? $scope.recipients.indexOf(blankRecords[0]) : 0;
 			updateForm();
-		};
-
-		//Allows user to close an alert
-		$scope.closeAlert = function() {
-			$scope.activateAlert = false;
 		};
 
 		//Helps find how many days are left until the deadline
@@ -639,17 +649,16 @@ angular.module('letters').controller('AgencyController',
 		}
 
 		//Allows user to clear the current slot
-		$scope.clearForm = function() {
+		$scope.clearForm = function(form) {
 			$scope.current.name = '';
 			$scope.current.age = '';
 			$scope.current.gender = '';
 			$scope.current.gift = '';
-			$scope.current.$update();
 		};
 
 		//Helps to show user appropriate age range of each recipient type
 		function updateForm() {
-			$scope.current = $scope.recipients[$scope.currentIndex];
+			$scope.current = $scope.recipients[currentIndex];
 			if($scope.current.track.match(/^...C/)) {
 				$scope.minAge = 4;
 				$scope.maxAge = 13;
@@ -662,75 +671,92 @@ angular.module('letters').controller('AgencyController',
 				$scope.minAge = 65;
 				$scope.maxAge = 125;
 			}
-		};
+		}
 
-		//Allow user to see/edit the next record
+		//Allow user to see/edit the next record if current letter is valid
 		$scope.goToNext = function(form) {
-			checkForm(form);
-			if($scope.allValid) {
-				addRecipient(form);
-				if($scope.currentIndex < $scope.recipients.length - 1) {
-					$scope.currentIndex++;
+			if(isValidLetter(form)) {
+				if(currentIndex < $scope.recipients.length - 1) {
+					currentIndex++;
 					updateForm();
+				}
+				else {
+					$scope.alert = {
+						active: true,
+						  type: 'info',
+						   msg: 'You just entered the last letter on this page.'
+					};
 				}
 			}
 		};
 
-		//Allow user to see the record they selected
+		//Allow user to see the record they selected if current letter is valid
 		$scope.goToSelected = function(selected, form) {
-			checkForm(form);
-			if($scope.allValid) {
-				addRecipient(form);
-				$scope.currentIndex = $scope.recipients.indexOf(selected);
+			if(isValidLetter(form)) {
+				currentIndex = $scope.recipients.indexOf(selected);
 				updateForm();
 			}
 		};
 
-		//Help validate user's data entry
-		function checkForm(form) {
-			$scope.blankName = !form.$pristine && !$scope.current.name ? true : false;
-			$scope.invalidAge = !form.age.$pristine && ($scope.current.age < $scope.minAge || $scope.current.age > $scope.maxAge) ? true : false;
-			if(!form.gender.$pristine) {
-				$scope.invalidGender = $scope.current.gender.match(/^[fmFM]$/) ? false : true;
-			}
-			$scope.allValid = $scope.blankName || $scope.invalidGender || $scope.invalidAge ? false : true;
-			//Require all fields
-			if(!form.$pristine && (!$scope.current.name || !$scope.current.age || !$scope.current.gender || !$scope.current.gift)) {
-				$scope.allValid = false;
-			}
-			//Allow user to proceed if he/she clears all fields
-			if(!$scope.current.name && !$scope.current.age && !$scope.current.gender && !$scope.current.gift) {
+		//Make form more user-friendly, make required fields glow
+		$scope.isUsed = function(form) {
+			if($scope.current.name.length > 0) {
 				$scope.blankName = false;
-				$scope.invalidGender = false;
-				$scope.invalidAge = false;
-				$scope.allValid = true;
+				form.age.$setTouched();
+				form.gender.$setTouched();
+				form.gift.$setTouched();
+			}
+			else {
+				form.$setUntouched();
+			}
+		};
+
+		//Check if age entered is within valid range
+		$scope.isWithinRange = function(age) {
+			age.$setValidity('inRange', $scope.current.age === null || ($scope.current.age >= $scope.minAge && $scope.current.age <= $scope.maxAge));
+		};
+
+		//Help validate user's data entry
+		function isValidLetter(form) {
+			//It's OK if no data was entered
+			if(!$scope.current.name && !$scope.current.age && !$scope.current.gender && !$scope.current.gift) {
+				return true;
+			}
+			//It's not OK if some fields are missing
+			else if(!$scope.current.name || !$scope.current.age || !$scope.current.gender || !$scope.current.gift) {
+				$scope.blankName = !$scope.current.name ? true : false;
+				$scope.error = 'fields cannot be left blank';
+				$timeout(function() {
+					$scope.blankName = false;
+					$scope.error = null;
+				}, 2000);
+				return false;
+			}
+			//It's great when all fields are entered properly
+			else {
+				addRecipient(form);
+				return true;
 			}
 		}
 
 		//Helps update/add recipient record
 		function addRecipient(form) {
-			if(!form.$pristine && ($scope.current.name || (!$scope.current.name && !$scope.current.age && !$scope.current.gender && !$scope.current.gift))){
-				$scope.current.name = cleanText($scope.current.name, 1).trim();
-				$scope.current.gender = $scope.current.gender.toUpperCase();
-				$scope.current.gift = cleanText($scope.current.gift, 2);
+			$scope.current.name = cleanText($scope.current.name, 1).trim();
+			$scope.current.gender = $scope.current.gender.toUpperCase();
+			$scope.current.gift = cleanText($scope.current.gift, 2);
 
-				//update Agency status
-				if(!$scope.adminView) {
-					blankRecords = $filter('filter')(Recipients, {updated: ''});
-					$scope.currentAgency.status = blankRecords.length > 0 ? 1 : 2;
-					var user = new Users($scope.currentAgency);
-					user.$update(function(response) {
-						$scope.currentAgency = response;
-					}, function(response) {
-						$scope.error = response.data.message;
-					});
-				}
-
-				$scope.current.$update();
-				console.log('saved');
-
-				form.$setPristine();
+			//update Agency status
+			if($scope.currentAgency.status === 0) {
+				$scope.currentAgency.status = 1;
+				var user = new Users($scope.currentAgency);
+				user.$update(function(response) {
+					$scope.currentAgency = response;
+				});
 			}
+
+			$scope.current.$update();
+
+			form.$setUntouched();
 		}
 
 		//Helps clean up sloppy user input
@@ -746,15 +772,16 @@ angular.module('letters').controller('AgencyController',
 			}
 		}
 
-		//Allows partner to submit their completed tracking form
 		//Allows admin to complete the review of a tracking form
+		//Allows community partner to submit their completed tracking form
 		$scope.confirmCompletion = function () {
 			var dblcheck = null;
+			var user = null;
 			if($scope.adminView) {
 				dblcheck = confirm('Click OK to confirm that you have reviewed this tracking form.');
 				if(dblcheck) {
 					$scope.currentAgency.status = 5;
-					var user = new Agencies($scope.currentAgency);
+					user = new Agencies($scope.currentAgency);
 					user.$update(function(response) {
 						$scope.currentAgency = response;
 						downloadCSV();
@@ -767,7 +794,7 @@ angular.module('letters').controller('AgencyController',
 				dblcheck = confirm('Click OK to let the Winter Wishes Team know that your tracking form is ready. You will not be able to make any further changes.');
 				if(dblcheck) {
 					$scope.currentAgency.status = 3;
-					var user = new Users($scope.currentAgency);
+					user = new Users($scope.currentAgency);
 					user.$update(function(response) {
 						$scope.currentAgency = response;
 						downloadCSV();
@@ -812,7 +839,7 @@ angular.module('letters').controller('AgencyController',
 			if($scope.adminView) {headers.push('flagged');}
 			var csvString= headers.join(',') + '\r\n';
 			for (var i=0; i < Recipients.length; i++) {
-				if(Recipients[i]['name']) {
+				if(Recipients[i].name) {
 					for(var key in headers) {
 						var line = Recipients[i][headers[key]];
 						if(key === 4) {
@@ -826,9 +853,8 @@ angular.module('letters').controller('AgencyController',
 				}
 			}
 
-			var date = new Date();
-			var stamp = (date.getMonth() + 1) + '-' + date.getDate();
-			$scope.fileName = ( 'WishesToSF_' + stamp + '.csv' );
+			var date = $filter('date')(new Date(), 'MM-dd');
+			$scope.fileName = ( 'WishesToSF_' + date + '.csv' );
 			var blob = new Blob([csvString], { type: 'text/csv;charset=UTF-8' }); 
 			$scope.url = window.URL.createObjectURL( blob );
 		}
@@ -838,37 +864,6 @@ angular.module('letters').controller('AgencyController',
 			selected.received = !selected.received;
 			selected.$update();
 		};
-
-/*
-		//
-		//$scope.countdown = dateDiff(new Date(), new Date(due));
-
-		if($scope.countdown === 14) {
-			$scope.alert = {type: 'warning', msg: 'Two weeks left'};
-		}
-		else if($scope.countdown === 7) {
-			$scope.alert = {type: 'warning', msg: 'One week left'};
-		}
-		else if($scope.countdown === 0) {
-			$scope.alert = {type: 'danger', msg: 'Last day to submit'};
-		}
-		else if($scope.countdown === 1) {
-			$scope.alert = {type: 'danger', msg: 'One day left'};
-		}
-		else if($scope.countdown < 0) {
-			$scope.alert = {type: 'danger', msg: 'Past due -- please submit it ASAP'};
-		}
-		else if($scope.countdown <= 3) {
-			$scope.alert = {type: 'danger', msg: $scope.countdown + ' days left'};
-		}
-
-		$scope.activateAlert = $scope.alert === null ? false : true;
-
-			});
-		});
-	}]
-)*/
-
 
 }]);
 'use strict';
@@ -883,12 +878,14 @@ angular.module('letters').factory('Agencies', ['$resource',
 		});
 	}
 ]);
+'use strict';
+
 angular.module('letters').directive('donutChart', function() {
 	return {
 		restrict: 'E',
 		scope: { data: '=' },
-		link: function(scope, element) {
-			var element = element[0];
+		link: function(scope, elem) {
+			var element = elem[0];
 			var margin = {top: 20, right: 30, bottom: 30, left: 55},
 			width = element.clientWidth - margin.left - margin.right,
 			height = 300 - margin.top - margin.bottom;
@@ -936,7 +933,7 @@ angular.module('letters').directive('donutChart', function() {
 				}
 			}, true);
 		}
-	}
+	};
 });
 'use strict';
 
@@ -1035,22 +1032,8 @@ angular.module('users').config(['$stateProvider',
 
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
-		$scope.authentication = Authentication;
+		$scope.user = Authentication.user;
 
-		// If user is signed in then redirect back home
-		if($scope.authentication.user) redirect($scope.authentication.user);
-
-		$scope.signin = function() {
-			$http.post('/auth/signin', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
-				// And redirect to appropriate page
-				redirect(response);
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
-		
 		function redirect(user) {
 			if(user.username === 'AAA') {
 				$location.path('/admin');
@@ -1059,6 +1042,20 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				$location.path('/agency/' + user._id);
 			}
 		}
+
+		// If user is signed in then redirect back home
+		if($scope.user) redirect($scope.user);
+
+		$scope.signin = function() {
+			$http.post('/auth/signin', $scope.credentials).success(function(response) {
+				// If successful we assign the response to the global user model
+				Authentication.user = response;
+				// And redirect to appropriate page
+				redirect(response);
+			}).error(function(response) {
+				$scope.error = response.message;
+			});
+		};
 
 		$scope.signup = function() {
 			$http.post('/auth/signup', $scope.credentials).success(function(response) {
