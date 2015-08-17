eb253481d78f7755e7dbdb9cb02874630fb70c45
Elmer Meza <meza.elmer@gmail.com>
2015-08-16 22:50:41 -0400

    improved letter form
    -added labels to avoid having to delete to make sure info is being
    typed in the right place
    -gender input now uses radio buttons to avoid unnecessary data
    validation
    -removed unnecessary code for file upload modal

--- public/modules/letters/controllers/fileuploadmodal.js
+++ public/modules/letters/controllers/fileuploadmodal.js
@@ -21,46 +21,6 @@ angular.module('letters').controller('MappingModalCtrl', ['$state', '$scope', '$
             }
         }
 
-        // $scope.create = function() {
-        //     var article = new Mapping({
-        //         type: arrays.type,
-        //         map: $scope.model.lists.B
-        //     });
-        //     article.$save(function(response) {
-        //         console.log('map saved');
-        //     }, function(errorResponse) {
-        //         $scope.error = errorResponse.data.message;
-        //     });
-        // };
-
-        // $scope.getMapping = function() {
-        //     if ($scope.model.lists.B.length) {
-        //         $scope.model.lists.A = $scope.model.lists.A.concat($scope.model.lists.B);
-        //     }
-        //     $scope.model.lists.B = [];
-        //     Mapping.query(function(map) {
-        //         var index = _.findIndex(map, {
-        //             'type': arrays.type
-        //         });
-        //         var savedMap = map[index].map;
-        //         for (var i = 0; i < $scope.required_fields.length; i++) {
-        //             var isValidColumn = _.findIndex($scope.model.lists.A, {
-        //                 'label': savedMap[i].label
-        //             });
-        //             if (isValidColumn > -1) {
-        //                 $scope.model.lists.B.push(savedMap[i]);
-        //                 $scope.model.lists.A.splice(isValidColumn, 1);
-        //             }
-        //         }
-        //         if ($scope.model.lists.B.length < $scope.required_fields.length) {
-        //             console.log('some required fields appear to be missing from your csv file');
-        //         } else {
-        //             $scope.isSavedMapping = true;
-        //         }
-
-        //     });
-        // };
-
         $scope.exitModal = function() {
             if ($scope.model.lists.B.length === $scope.required_fields.length) {
                 $modalInstance.close($scope.model.lists.B);
